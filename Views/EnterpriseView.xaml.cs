a1c2bf09af837facebecd42a2286796ec190cd90
Mr_Zet <gesmannikita@gmail.com>
2025-10-29 00:10:13 +0300

    Update EnterpriseView

--- Views/EnterpriseView.xaml.cs
+++ Views/EnterpriseView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 using PDSCalculatorDesktop.ViewModels;
 
 namespace PDSCalculatorDesktop.Views
@@ -8,8 +11,33 @@ namespace PDSCalculatorDesktop.Views
         public EnterpriseView(EnterpriseViewModel viewModel)
         {
             InitializeComponent();
-
             DataContext = viewModel;
+        }
+
+        // Обработчик для снятия выделения при клике на пустое место
+        private void DataGrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is DataGrid dataGrid)
+            {
+                // Проверяем, кликнули ли мы НЕ на строку
+                var hitTestResult = VisualTreeHelper.HitTest(dataGrid, e.GetPosition(dataGrid));
+                if (hitTestResult != null)
+                {
+                    var element = hitTestResult.VisualHit;
+
+                    // Ищем родительский DataGridRow
+                    while (element != null && element is not DataGridRow && element != dataGrid)
+                    {
+                        element = VisualTreeHelper.GetParent(element);
+                    }
+
+                    // Если не нашли DataGridRow (т.е. кликнули в пустое место)
+                    if (element is not DataGridRow)
+                    {
+                        dataGrid.UnselectAll();
+                    }
+                }
+            }
         }
     }
 }
